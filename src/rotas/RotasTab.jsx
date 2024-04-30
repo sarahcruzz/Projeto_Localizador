ad064e41e6eb69066cdec02b9302f5ff7260b3a9
sarahcruzz <sarah.cruz0492@gmail.com>
2024-04-30 16:51:47 -0300

    adicionando interação

--- src/rotas/RotasTab.jsx
+++ src/rotas/RotasTab.jsx
@@ -2,26 +2,53 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { Inicial } from "../telas/Inicial"
 import { Usuarios } from "../telas/Usuarios"
 import { AmbientesEquipamentos } from "../telas/AmbientesEquipamentos"
+import { FontAwesome5 } from '@expo/vector-icons'
 
 const { Navigator, Screen } = createBottomTabNavigator()
 
 export function RotasTab(){
     return(
-        <Navigator>
+        <Navigator screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false,
+            tabBarActiveTintColor: '#fff',
+            tabBarInactiveTintColor: '#6c757d',
+            tabBarStyle: {
+                backgroundColor: '#560bad',
+                borderTopColor: 0,
+                paddingBottom: 10,
+                paddingTop: 10
+            }
+        }}>
 
             <Screen  
                 name="inicial"
                 component={Inicial}
+                options={{ 
+                    tabBarIcon: ({color, size}) => (
+                        <FontAwesome5 name="home" color={color} size={size} />
+                    )
+                }}
             />
 
             <Screen  
                 name="usuarios"
                 component={Usuarios}
+                options={{ 
+                    tabBarIcon: ({color, size}) => (
+                        <FontAwesome5 name="user" color={color} size={size} />
+                    )
+                }}
             />
 
             <Screen  
                 name="ambiente"
                 component={AmbientesEquipamentos}
+                options={{ 
+                    tabBarIcon: ({color, size}) => (
+                        <FontAwesome5 name="arrow-up" color={color} size={size} />
+                    )
+                }}
             />
 
         </Navigator>
